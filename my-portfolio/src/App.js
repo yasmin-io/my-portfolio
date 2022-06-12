56ba20f2efa70009c62df471b495d6e9637c4d8b
Yasmin <yasmin.bunnie@gmail.com>
2022-06-13 00:21:21 +0100

    added basic files and folders

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
