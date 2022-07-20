730c0aaa0baf9cee78a2feb31e68127e92537577
Yasmin <yasmin.bunnie@gmail.com>
2022-07-20 02:41:29 +0100

    removed unused code and installed new packages

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./styles/fonts";
+
 import Header from "./components/header";
 import Home from "./pages/Home";
 
--- my-portfolio/src/components/navbar.js
+++ my-portfolio/src/components/navbar.js
@@ -1,25 +1,28 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 
-import { Container, Navbar, Nav } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
 import Logo from "../images/IMG_1961.png";
 
 function NavBar() {
-  const [activeLink, setActiveLink] = useState('home');
+  const [activeLink, setActiveLink] = useState("home");
   const [scrolled, setScrolled] = useState(false);
 
   useEffect(() => {
-    // If the user Scrolls... 
+    // If the user Scrolls...
     const onScroll = () => {
       if (window.scrollY > 50) {
         // Set Scrolled to true.
         setScrolled(true);
-      } else { // If not...
+      } else {
+        // If not...
         // Set scrolled to false.
         setScrolled(false);
       }
-    }
+    };
 
-    
     // Adding an eventlistener to the window to detect when the user scrolls
     window.addEventListener("scroll", onScroll());
 
@@ -27,9 +30,13 @@ function NavBar() {
     return () => window.removeEventListener("scroll", onScroll());
   }, []);
 
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  };
+
   return (
     <>
-      <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
+      <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Brand href="#home">
             <img
@@ -42,16 +49,45 @@ function NavBar() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#about-me">About Me</Nav.Link>
-              <Nav.Link href="#projects">Projects</Nav.Link>
+              <Nav.Link
+                href="#home"
+                className={
+                  activeLink === "home" ? "active-navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("home")}
+              >
+                Home
+              </Nav.Link>
+              <Nav.Link
+                href="#about-me"
+                className={
+                  activeLink === "about-me"
+                    ? "active-navbar-link"
+                    : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("about-me")}
+              >
+                About Me
+              </Nav.Link>
+              <Nav.Link
+                href="#projects"
+                className={
+                  activeLink === "projects"
+                    ? "active-navbar-link"
+                    : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("projects")}
+              >
+                Projects
+              </Nav.Link>
             </Nav>
             <span className="navba-text">
-              <div className="social-icon">
-                <a href="#"><img src={} alt=""></img></a>
-                <a href="#"><img src={} alt=""></img></a>
-              </div>
-              <button className="cv-button" onClick={() => console.log('download cv')}>CV Download</button>
+              <button
+                className="cv-button"
+                onClick={() => console.log("download cv")}
+              >
+                CV Download
+              </button>
             </span>
           </Navbar.Collapse>
         </Container>
